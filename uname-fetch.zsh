7cc284f45f044be0eb45877bc26f217f4fadc906
Jack Chatelain <16484281+jackchatelain@users.noreply.github.com>
2024-07-06 00:57:46 +0900

    Create uname-fetch.zsh

---
+++ uname-fetch.zsh
@@ -0,0 +1,16 @@
+echo    "--- System ---"
+echo -n "System: ";       uname -o
+echo -n "Hostname: ";     uname -n
+echo    "--------------"
+echo
+echo    "----- CPU ----"
+echo -n "Architecture: "; uname -m
+echo -n "Type: ";         uname -p
+echo    "--------------"
+echo
+echo    "--- Kernel ---"
+echo -n "Type: ";         uname -s
+echo -n "Version: ";      uname -r
+echo -n "Version date:";  uname -v | sed 's/:/\n/' | tr ';' '\n' | awk 'NR==2'
+echo -n "Version info:";  uname -v | sed 's/:/\n/' | tr ';' '\n' | awk 'NR==3'
+echo    "--------------"
